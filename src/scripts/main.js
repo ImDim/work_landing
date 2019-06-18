e51e83c03bdf5599d2273c24f2b37644787a3720
Dmitriy Rybkin <theimdim@gmail.com>
2019-06-18 23:42:00 +0400

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+import Swiper from './swiper';
+
+$(function() {
+    var swiper = new Swiper('.swiper-container', {
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+})
